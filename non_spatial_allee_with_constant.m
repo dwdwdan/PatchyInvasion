d2d865fdf344aa01e8305dad51d5c7ad3b4ed3dd
Daniel Walters <dan.walters5@outlook.com>
2023-10-20 14:19:31 +0100

    Find Solution for allee model with a constant

---
+++ non_spatial_allee_with_constant.m
@@ -0,0 +1,36 @@
+% This script solves the non-spatial ODEs considering the Strong Allee Effect.
+clear
+close all
+
+%% Function Parameters
+K = 1000;               % Carrying Capacity
+alpha = 0.01;
+beta = 200;
+tmax = 10;              % Time at end of interest
+num_timesteps = 100;     % Number of timesteps to use
+epsilon = 1;
+
+dt=tmax/num_timesteps;  % Time step
+
+
+
+cs = -300:100:200;
+u0s = [0:50:500 500:200:1500];
+convergence_times = [];
+tiledlayout("flow");
+for c = cs
+    nexttile
+    allee_diff = @(t,u) alpha*u * (u-beta) * (1-u/K) + c;
+    hold on
+    for u0 = u0s
+        [t, u_num] = ode45(allee_diff, 0:dt:tmax, u0);
+        plot(t, u_num);
+        title("\(c = " + c + "\)", 'Interpreter','latex')
+        xlabel("Time")
+        ylabel("Population")
+        ylim([0 1500]);
+        yticks(0:200:1500)
+    end
+    hold off
+
+end
