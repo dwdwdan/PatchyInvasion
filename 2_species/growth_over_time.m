a823a3701c566ea1a5c56229d249acc77f2e0386
Daniel Walters <dan.walters5@outlook.com>
2023-11-14 17:53:59 +0000

    Clean the growth over time graph
    Natalia mentioned in our meeting that the solution to the ugly
    oscillations is to assume any populations less that 10^-6 are 0

--- 2_species/growth_over_time.m
+++ 2_species/growth_over_time.m
@@ -9,6 +9,21 @@ f = @(t,W) [W(1)*(1-W(2)); W(2)*(1-W(1))];
 
 [ts, ws] = ode45(f, [0, tmax], [u0;v0]);
 
+%% Cleaning
+% We assume that any population less that 10^-6 is 0, in order to remove
+% artifacts of the numerical method
+
+for idx = 1:size(ws, 1)
+    if ws(idx, 2) < 1e-6
+        ws(idx, 2) = 0;
+    end
+    if ws(idx, 1) < 1e-6
+        ws(idx, 1) = 0;
+    end
+end
+
+%% Plotting
+
 fig=figure();
 semilogy(ts, ws(:, 1));
 hold on
