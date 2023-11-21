b42f2e99b3a015dc1ce900da14dda3eb8e9fbd59
Daniel Walters <dan.walters5@outlook.com>
2023-11-21 12:18:17 +0000

    Compute stability of steady states

--- main_model/non-spatial/steady_states.m
+++ main_model/non-spatial/steady_states.m
@@ -1,5 +1,8 @@
-clear all
+clear
 syms u v t gamma beta m
+assume(gamma, "real")
+assume(beta, "real")
+assume(m, "real")
 
 u_rhs = gamma*u*(u-beta)*(1-u) - u*v;
 v_rhs = u*v - m*v;
@@ -10,5 +13,17 @@ hor_iso_v = solve(u_rhs == 0, v);
 vert_iso_u = solve(v_rhs == 0, u);
 vert_iso_v = solve(v_rhs == 0, v);
 
-steady_states = solve(u_rhs == 0, v_rhs == 0);
+equilibria = solve(u_rhs == 0, v_rhs == 0);
 
+u_eqs = equilibria.u;
+v_eqs = equilibria.v;
+
+eqs = [u_eqs v_eqs];
+
+J = jacobian([u_rhs; v_rhs], [u v]);
+
+eigs = [];
+for eq=eqs'
+    eig_val = eig(subs(J, [u v], [eq(1), eq(2)]));
+    eigs = [eigs eig_val];
+end
