71c941fe9d92e13ee13eedbb3d45e56cb48dd698
Daniel Walters <dan.walters5@outlook.com>
2023-11-27 17:37:17 +0000

    Plot the dynamics of the non-spatial model over time

---
+++ main_model/non-spatial/growth_over_time.m
@@ -0,0 +1,35 @@
+clear
+close all
+
+tmax = 15;
+
+gamma = 5;
+beta = 0.5;
+m = 0.9;
+
+u0s = [1.5 1.5 1.5 0.6];
+v0s = [0.4 0.5 0.1 0.1];
+
+fig=figure();
+tiledlayout("flow");
+for idx = 1:length(u0s)
+    nexttile
+    [ts, ws] = ode45(@(t, W) f(W, gamma, beta, m), [0, tmax], [u0s(idx);v0s(idx)]);
+    
+    plot(ts, ws(:, 1));
+    hold on
+    plot(ts, ws(:, 2));
+    legend(["u","v"]);
+    xlabel("Time");
+    ylabel("Population")
+    title("\(u_0 = " + u0s(idx) + "\), \(v_0 = "+v0s(idx) + "\)", 'Interpreter','latex')
+    fontsize(fig, 18, "points")
+    ylim([0 1.2])
+
+
+end
+
+function W = f(W, gamma, beta, m)
+    [u, v] = model(W(1), W(2), gamma, beta, m);
+    W = [u;v];
+end
