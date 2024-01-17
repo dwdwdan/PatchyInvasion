7ea041c08647e8aea2502894ab42ea299126696f
Daniel Walters <dan.walters5@outlook.com>
2024-01-17 15:36:16 +0000

    Save the model parameters in the solution file

--- main_model/Intitial Investigations/allee_diffusion/get_solution.m
+++ main_model/Intitial Investigations/allee_diffusion/get_solution.m
@@ -5,6 +5,10 @@ X=100;
 
 epsilon = 1e-3;
 
-[xvec, tvec, umat] = solve_and_check(100,tmax,dx,dt,epsilon, 1,0.06,1);
+alpha = 1;
+beta = 0.6;
+K=1;
+
+[xvec, tvec, umat] = solve_and_check(100,tmax,dx,dt,epsilon, alpha, beta, K);
 
 save("solution.mat")
