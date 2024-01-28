a0ae168573d55b282d1fec2eb6fa1df7d1d11a35
Daniel Walters <dan.walters5@outlook.com>
2024-01-28 19:44:16 +0000

    Add horizontal lines for steady states

--- main_model/non-spatial/growth_over_time.m
+++ main_model/non-spatial/growth_over_time.m
@@ -19,12 +19,14 @@ for idx = 1:length(u0s)
     plot(ts, ws(:, 1));
     hold on
     plot(ts, ws(:, 2));
-    legend(["u","v"]);
     xlabel("Time");
     ylabel("Population")
     title("\(u_0 = " + u0s(idx) + "\), \(v_0 = "+v0s(idx) + "\)", 'Interpreter','latex')
     fontsize(fig, 18, "points")
     ylim([0 1.2])
+    yline(0.9, "--")
+    yline(0.2, "--")
+    legend(["u","v", "", ""]);
 
 
 end
